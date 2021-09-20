a97b7b86563767c58bebf94eaafcdc279abd4c12
Ezequiel Grillo <ezequiel.grillo@gmail.com>
2021-09-19 23:16:59 -0300

    Cambio Vacunas, tabla se carga por const y map

--- src/dashboard2/src/views/Vacunas/Vacunas.js
+++ src/dashboard2/src/views/Vacunas/Vacunas.js
@@ -17,7 +17,15 @@ import calendario from '../../assets/img/CalendarVac2020.jpg'
 import VacunasForm from "../../components/VacunasForm/VacunasForm.js";
 import IconButton from "@material-ui/core/IconButton";
 import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
-
+import RemoveRedEye from "@material-ui/icons/RemoveRedEye";
+import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from "@material-ui/icons/Close";
+import {
+  defaultFont,
+  primaryColor,
+  dangerColor,
+  grayColor,
+} from '../../assets/jss/material-dashboard-react'
 
 
 
@@ -43,9 +51,34 @@ const styles = {
     float: 'right',
     color: 'primary',
   },
+  edit: {
+    backgroundColor: "transparent",
+    color: primaryColor[0],
+    boxShadow: "none",
+  },
+  close: {
+    backgroundColor: "transparent",
+    color: dangerColor[0],
+    boxShadow: "none",
+  },
+  tableActionButton: {
+    width: "27px",
+    height: "27px",
+    padding: "0",
+  },
+  tableActionButtonIcon: {
+    width: "17px",
+    height: "17px",
+  },
 };
 
 const useStyles = makeStyles(styles);
+
+const tablaVacunas = [
+  { nombre: 'Dakota Rice', fecha: '01/02/2021', vacuna: 'Varicela', lugar: 'Lanus' , dosis: 'Unica'},
+  { nombre: 'Minerva Hooper', fecha: '08/01/2021', vacuna: 'Hepatitis B', lugar: 'Lomas de Zamora', dosis: 'Primera' },
+  { nombre: 'Dakota Rice', fecha: '12/02/2020', vacuna: 'BCG', lugar: 'CABA' , dosis: 'Refuerzo'},
+]
 
 export default function Vacunas() {
   const classes = useStyles();
@@ -60,6 +93,8 @@ export default function Vacunas() {
     //alert('HOLA');
     //console.log(e);
   }
+
+  const [vacunas, setVacunas] = useState(tablaVacunas);
 
   return (
     <div>
@@ -198,12 +233,31 @@ export default function Vacunas() {
               <CardBody>
                 <Table
                   tableHeaderColor="primary"
-                  tableHead={["Nombre", "Fecha", "Especialista", "Salary", "", ""]}
-                  tableData={[
-                    ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                    ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                    ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                  ]}
+                  tableHead={["Nombre", "Fecha", "Vacuna", "Lugar", "Dosis", "", ""]}
+                  tableData={
+                    vacunas.map((vacuna) => (
+                      [vacuna.nombre, 
+                      vacuna.fecha,
+                      vacuna.vacuna,
+                      vacuna.lugar,
+                      vacuna.dosis,
+                      <IconButton className={classes.tableActionButton}>
+                        <RemoveRedEye className={classes.tableActionButtonIcon + " " + classes.edit} />
+                      </IconButton>,
+                      <IconButton className={classes.tableActionButton}>
+                        <EditIcon className={classes.tableActionButtonIcon + " " + classes.edit} />
+                      </IconButton>,
+                      <IconButton className={classes.tableActionButton}>
+                        <CloseIcon className={classes.tableActionButtonIcon + " " + classes.close} />
+                      </IconButton>
+                      ]))   
+                  }
+                    
+                  // [
+                  //   ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
+                  //   ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
+                  //   ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
+                  // ]}
                 />
               </CardBody>
               <CardFooter>
